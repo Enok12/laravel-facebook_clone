f4c20b2c804378db35a682f11e210a7b7550baab
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-23 20:53:10 +0530

    Generated Factories Test Cases for POST and GET

---
+++ tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+
+
+
+class PostToTimelineTest extends TestCase
+{
+    use RefreshDatabase;
+   /** @test */
+   public function a_user_can_post_a_text_post(){
+
+    $this->withoutExceptionHandling();
+
+    $user = User::factory()->create();
+    $this->actingAs($user,'api');
+    $response = $this->post('/api/posts',[
+        'data' => [
+            'type' => 'posts',
+            'attributes' => [
+                'body' => 'Testing Body'
+            ] 
+        ]
+    ]);
+
+   
+
+    $post = Post::first();
+
+    $this->assertCount(1, Post::all()); 
+    $this->assertEquals($user->id, $post->user_id);
+    $this->assertEquals('Testing Body', $post->body);
+
+    $response->assertStatus(201)
+    ->assertJson([
+        'data' => [
+            'type' => 'posts',
+            'post_id' => $post->id,
+            'attributes' => [
+                'posted_by' => [
+                    'data' => [
+                        'attributes' => [
+                            'name' => $user->name
+                        ]
+                    ]
+                ],
+                'body' => 'Testing Body'
+            ] 
+            ],
+            'links' => [
+                'self' => url('/post/'.$post->id),
+            ]
+    ]);
+
+   }
+}
---
+++ tests/Feature/RetrievePostsTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RetrievePostsTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_user_can_retrieve_posts()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $this->actingAs($user,'api');
+
+        $posts = Post::factory(2)->create();
+        $response = $this->get('/api/posts');
+
+        $response->assertStatus(200)
+        ->assertJson([
+            'data' =>[
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $posts->first()->id,
+                        'attributes' => [
+                            'body' => $posts->first()->body,
+                        ]
+                    ]
+                ],
+
+                        [
+                            'data' => [
+                                'type' => 'posts',
+                                'post_id' => $posts->last()->id,
+                                'attributes' => [
+                                    'body' => $posts->last()->body,
+                                ]
+                            ]
+                        ]
+            ],
+
+            'links' => [
+                'self' => url('/post/'),
+            ]
+            
+        ]);
+
+
+
+    }
+}
