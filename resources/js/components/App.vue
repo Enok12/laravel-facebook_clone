75e1ee9061805e5328eecfeeccde76d084b852eb
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-09-26 11:30:36 +0530

    Sidebar and navigation Component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,18 +1,30 @@
 <template>
-    <div>Start
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+    <Nav />
+
+    <div class="flex overflow-y-hidden flex-1">
+      <Sidebar />
+
+      <div class="overflow-x-hidden w-2/3">
         <router-view></router-view>
+        <!-- <p v-for="i in 100">Testing</p> -->
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <script>
+import Nav from "./Nav.vue";
+import Sidebar from "./Sidebar.vue";
 
-export default ({
-    name:"App"
-})
-
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Sidebar,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
