827af45d336bbdf356429a51fb3f1cc62c598966
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-10-03 11:00:20 +0530

    Displaying Posts

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 let start = require('./components/views/start.vue').default;
+let NewsFeed = require('./components/views/NewsFeed.vue').default;
+
 
 Vue.use(VueRouter);
 
@@ -10,7 +12,7 @@ export default new VueRouter({
 
     routes:[
         {
-            path: '/',name:'home',component:start,
+            path: '/',name:'home',component:NewsFeed,
         }
     ]
 });
