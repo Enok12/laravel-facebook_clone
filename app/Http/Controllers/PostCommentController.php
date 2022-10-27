2e5f638733743224735383d436d6e444e51b269c
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-10-27 17:48:17 +0530

    Create Controller , Model, Resources, and initial test for Comments

---
+++ app/Http/Controllers/PostCommentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Http\Resources\CommentCollection as CommentCollection;
+
+
+class PostCommentController extends Controller
+{
+   public function store(Post $post){
+    $data = request()->validate([
+        'body' =>'',
+    ]);
+
+    $post->comments()->create(array_merge($data,[
+        'user_id' =>auth()->user()->id
+    ]));
+
+    return new CommentCollection($post->comments);
+   }
+}
