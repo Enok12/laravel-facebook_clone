1963320f29abc921c7709ff8f0c38528e5fecf9a
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2022-11-04 18:53:09 +0530

    Register Blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,10 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    Register
+                                </a>
                             </div>
                         </div>
                     </form>
